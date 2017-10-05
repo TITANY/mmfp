205881b8906873389ff33cf6e31ad5089bfd3a61
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-10-05 20:20:30 +0300

    Initial commit: electron+vue+vuetify boilerplate, + custom eslint config

---
+++ src/renderer/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import App from './App';
+import router from './router';
+
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+    components: { App },
+    router,
+    template: '<App/>'
+}).$mount('#app');
