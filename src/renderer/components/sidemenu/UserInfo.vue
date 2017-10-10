6814480a1645696a0dea2cd79b7f9b0a99cd563a
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-10-10 17:10:56 +0300

    Sketch for UserInfo component

--- src/renderer/components/sidemenu/UserInfo.vue
+++ src/renderer/components/sidemenu/UserInfo.vue
@@ -1,12 +1,21 @@
 <template>
-<v-list-tile>
-    <v-list-tile-action>
-        <v-icon>person</v-icon>
-    </v-list-tile-action>
-    <v-list-tile-content>
-        <v-list-tile-title>{{ user.name }}</v-list-tile-title>
-    </v-list-tile-content>
-</v-list-tile>
+<v-container class="user-info">
+    <v-layout row wrap align-center>
+        <v-flex xs2>
+            <v-icon class="title">person</v-icon>
+        </v-flex>
+        <v-flex xs10>
+            <div class="title">{{ user.name }}</div>
+        </v-flex>
+
+        <v-flex offset-xs2>
+            <div>
+                Тестирование <b class="green--text">пройдено</b>
+                <v-icon class="green--text">done</v-icon>
+            </div>
+        </v-flex>
+    </v-layout>
+</v-container>
 </template>
 
 <script>
@@ -18,3 +27,10 @@ export default {
     }
 };
 </script>
+
+<style>
+.user-info {
+    background-color: rgba(0,0,0,0.05);
+    border-right: teal 5px solid;
+}
+</style>
