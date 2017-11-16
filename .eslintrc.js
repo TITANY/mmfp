d7df9796ea4dacc31f9a391ca63e7e8efd440473
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-11-16 10:50:22 +0300

    Updated eslint configuration to use multiple var declarations

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,11 @@ module.exports = {
         'curly': 0,
         // allow async-await
         'generator-star-spacing': 0,
+        'indent': ['error', 4],
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
         'no-multiple-empty-lines': [2, { max: 2, maxEOF: 1, maxBOF: 0 }],
-        'indent': ['error', 4],
+        'one-var': 0,
         'semi': ['error', 'always'],
         'space-before-function-paren': ['error', {
             'anonymous': 'always',
